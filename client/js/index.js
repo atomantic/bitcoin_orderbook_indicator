0e67cd589161d311e6be943b148f749b1063e7ba
Adam Eivy <atomantic@gmail.com>
2019-06-16 19:53:23 -0700

    add 6h to date fields

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const Vue = require('vue/dist/vue.js');
 const processLog = require('./process.log');
 const socket = io();
-const dateFields = ['date', 'hour', 'minute', 'day'];
+const dateFields = ['date', 'hour', 'minute', 'day', '6h'];
 
 const vueData = {
   connected: false,
